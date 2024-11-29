ce6de5d1a4559bfe3ecdc4fb561543fc5475dd78
ohayo <qkzkel17@naver.com>
2024-11-29 12:07:17 +0900

    팝스폿DB

---
+++ PopSpotDB/FAQ.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 화요일-11월-26-2024   
+--------------------------------------------------------
+REM INSERTING into FAQ
+SET DEFINE OFF;
+Insert into FAQ (FAQ_NO,ANSWER,CREATED_DATE,IS_DELETED,DELETED_DATE,MODIFIED_DATE,QUESTION,USER_ID) values (1,'ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄹ',to_date('24/09/23','RR/MM/DD'),1,to_timestamp('24/09/26 20:50:56.065020000','RR/MM/DD HH24:MI:SSXFF'),null,'ㅁㄴㅇㄹ','admin2');
+Insert into FAQ (FAQ_NO,ANSWER,CREATED_DATE,IS_DELETED,DELETED_DATE,MODIFIED_DATE,QUESTION,USER_ID) values (21,'Pop-up 행사 리스트에서 원하시는 주제의 태그를 선택하시면 회원님이 원하는 종류의 주제만 모아서 보실 수 있습니다!',to_date('24/09/26','RR/MM/DD'),0,null,null,'원하는 주제의 팝업스토어들만 모아서 보고싶어요. 어떻게 해야 하나요?','test01');
+Insert into FAQ (FAQ_NO,ANSWER,CREATED_DATE,IS_DELETED,DELETED_DATE,MODIFIED_DATE,QUESTION,USER_ID) values (22,'팝업스토어 홍보 글은 기획자 계정으로 회원가입하면 등록 권한이 부여됩니다. 
+기획자 계정으로 로그인한 후, Pop-up 페이지의 ''등록'' 버튼을 눌러 행사 내용을 작성할 수 있습니다.',to_date('24/09/26','RR/MM/DD'),0,null,null,'팝업스토어 홍보 글을 작성하고 싶습니다. 어떻게 할 수 있을까요?','test01');
+Insert into FAQ (FAQ_NO,ANSWER,CREATED_DATE,IS_DELETED,DELETED_DATE,MODIFIED_DATE,QUESTION,USER_ID) values (41,'취미수정',to_date('24/09/27','RR/MM/DD'),1,to_timestamp('24/09/27 11:37:45.903627000','RR/MM/DD HH24:MI:SSXFF'),to_date('24/09/27','RR/MM/DD'),'취미는 무엇인가요?','test01');
